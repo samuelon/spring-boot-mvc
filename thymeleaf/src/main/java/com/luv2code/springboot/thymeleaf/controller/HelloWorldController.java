405f1fb4cfc3f51b47261733d8f9320d8c2e4cf1
samuelon <samuelon3@gmail.com>
2025-05-14 14:00:03 -0400

    added some data to spring model, processed it and displayed it

--- thymeleaf/src/main/java/com/luv2code/springboot/thymeleaf/controller/HelloWorldController.java
+++ thymeleaf/src/main/java/com/luv2code/springboot/thymeleaf/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymeleaf.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,4 +22,22 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    // need a controller method to read form data and
+    // add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model theModel){
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        //convert the data to all caps
+        theName = theName.toUpperCase();
+        //create the message
+        StringBuilder result = new StringBuilder("Yo! " + theName);
+
+        // add message to the model
+        theModel.addAttribute("message", result.toString());
+
+        return "helloworld";
+    }
 }
